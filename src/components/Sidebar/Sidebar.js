2990c2676ebf8106815275d85fdff227b02dbdbc
Quang Ngoc <ngocquang2509@gmail.com>
2022-08-09 17:33:56 +0700

    Implement Sidebar #6

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LeftSideBar, BrandName } from "./style";
+
+function SideBar() {
+  return (
+    <LeftSideBar>
+      <BrandName>Brand</BrandName>
+    </LeftSideBar>
+  );
+}
+
+export default SideBar;
---
+++ src/components/Sidebar/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const LeftSideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const BrandName = styled.p`
+  font-size: 32px;
+`;
+
+export { LeftSideBar, BrandName };
