fef5b5ceaa38677459970021199c5849bfc04e35
ngocquang2509 <ngocquang2509@gmail.com>
2022-08-10 17:39:36 +0700

    Create storybook for side bar

---
+++ src/components/Sidebar/Sidebar.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SideBar from "./Sidebar";
+
+export default {
+    title: 'Components/Sidebar',
+    component: SideBar,
+}
+
+export const Default = () => {
+    return <SideBar />
+}
