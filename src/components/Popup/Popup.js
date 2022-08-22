f9d15b0d3d6e7e5821d7bec35b94c28f774fb000
ngocquang2509 <ngocquang2509@gmail.com>
2022-08-22 17:33:19 +0700

    update modal #26

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,33 +1,77 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import * as SC from "./style";
 import Button from "../Button/Button";
+import { useStore, actions } from "../../store";
 //import Button from "../Button/Button";
+// import { useLocalStorage } from "../../store";
 
-const Popup = ({ title, label, isShow, hide }) =>
-  isShow
-    ? ReactDOM.createPortal(
-        <React.Fragment>
-          <SC.PopupOverlay />
-          <SC.PopupWrapper>
-            <SC.PopupClose onClick={hide}>X</SC.PopupClose>
-            <SC.PopupTitle>Modal{/* {title} */}</SC.PopupTitle>
-            <SC.PopupBody>
-              <SC.InputContainer>
-                <SC.PopupLabel>Name{/* {label} */}</SC.PopupLabel>
-                <SC.PopupInput />
-              </SC.InputContainer>
-            </SC.PopupBody>
-            <SC.PopupFooter>
-              <Button label="Cancel" backgroundColor="red" handleClick={hide}/>
-              <Button label="Create" backgroundColor="green" />
-            </SC.PopupFooter>
-          </SC.PopupWrapper>
-        </React.Fragment>,
-        document.body
-      )
-    : null;
+function Popup({ title, label, closeModal }) {
+
+  function onSubmit(e) {
+    e.preventDefault();
+    closeModal(false);
+  }
+
+  const [state, dispatch] = useStore();
+  const { items, itemInput } = state;
+
+  function handlAddItem() {
+    dispatch(actions.addItem(itemInput));
+  }
+
+  console.log("items", items);
+
+  return (
+    <>
+      <SC.PopupOverlay />
+      <SC.PopupWrapper>
+        <SC.PopupClose onClick={() => closeModal(false)}>X</SC.PopupClose>
+        <SC.PopupTitle>Modal{/* {title} */}</SC.PopupTitle>
+        <SC.PopupForm onSubmit={onSubmit}>
+          <SC.PopupBody>
+            <SC.InputContainer>
+              <SC.PopupLabel>Name{/* {label} */}</SC.PopupLabel>
+              <SC.PopupInput
+                type="text"
+                value={itemInput.itemName}
+                placeholder="Item Name"
+                onChange={(e) => {
+                  dispatch(actions.setItemInput(e.target.value));
+                }}
+              />
+            </SC.InputContainer>
+            <SC.InputContainer>
+              <SC.PopupLabel>Price{/* {label} */}</SC.PopupLabel>
+              <SC.PopupInput
+                type="text"
+                value={itemInput.itemPrice}
+                placeholder="Item Price"
+                onChange={(e) => {
+                  dispatch(actions.setItemInput(e.target.value));
+                }}
+              />
+              {/* onChange={onChangeItem} value={itemValues.itemPrice} name='itemPrice' */}
+            </SC.InputContainer>
+          </SC.PopupBody>
+          <SC.PopupFooter>
+            <Button
+              label="Cancel"
+              backgroundColor="red"
+              handleClick={() => closeModal(false)}
+            />
+            <Button
+              type="submit"
+              label="Create"
+              backgroundColor="green"
+              handleClick={handlAddItem}
+            />
+          </SC.PopupFooter>
+        </SC.PopupForm>
+      </SC.PopupWrapper>
+    </>
+  );
+}
 
 Popup.propTypes = {
   title: PropTypes.string,
--- src/store/hooks.js
+++ src/store/hooks.js
@@ -1,20 +1,7 @@
-import {useContext, useState} from 'react';
+import {useContext, useState, useEffect} from 'react';
 import Context from './Context';
 
 export const useStore = () => {
     const [state, dispatch] = useContext(Context);
     return [state, dispatch];
-}
-
-export const useModal = () => {
-    const [isShow, setIsShow] = useState(false);
-
-    function toggle() {
-        setIsShow(!isShow);
-    }
-
-    return {
-        isShow,
-        toggle,
-    }
 }
