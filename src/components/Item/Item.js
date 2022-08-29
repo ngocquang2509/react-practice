a9d3a5d010299a2bfab3112f49e339127f95c4d4
Quang Ngoc <ngocquang2509@gmail.com>
2022-08-29 17:11:29 +0700

    Fix issue #31

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import * as SC from "./style";
 import Button from "../Button/Button";
@@ -6,28 +6,29 @@ import Button from "../Button/Button";
 function Item({ name, price, category, image, products }) {
   return (
     <>
-      {products?.map((product, index) => (
-        <SC.WrappItem key={index}>
-          <SC.ItemImageWrapper>
-            <SC.ItemImg src={product.image} />
-          </SC.ItemImageWrapper>
-          <SC.ItemName>{product.name}</SC.ItemName>
-          <SC.ItemCategory as="p">{product.category}</SC.ItemCategory>
-          <SC.ItemPrice as="p">{product.price} VND</SC.ItemPrice>
-          <SC.Container>
-            <Button
-              label="Edit"
-              backgroundColor="#ffcf00"
-              img="/icons/edit.svg"
-            />
-            <Button
-              label="Delete"
-              backgroundColor="#ff0000"
-              img="/icons/delete.svg"
-            />
-          </SC.Container>
-        </SC.WrappItem>
-      ))}
+      {products &&
+        products.map((product, index) => (
+          <SC.WrappItem key={index}>
+            <SC.ItemImageWrapper>
+              <SC.ItemImg src={product.image} />
+            </SC.ItemImageWrapper>
+            <SC.ItemName>{product.name}</SC.ItemName>
+            <SC.ItemCategory as="p">{product.category}</SC.ItemCategory>
+            <SC.ItemPrice as="p">{product.price} VND</SC.ItemPrice>
+            <SC.Container>
+              <Button
+                label="Edit"
+                backgroundColor="#ffcf00"
+                img="/icons/edit.svg"
+              />
+              <Button
+                label="Delete"
+                backgroundColor="#ff0000"
+                img="/icons/delete.svg"
+              />
+            </SC.Container>
+          </SC.WrappItem>
+        ))}
     </>
   );
 }
@@ -36,6 +37,7 @@ Item.propTypes = {
   name: PropTypes.string,
   price: PropTypes.number,
   category: PropTypes.string,
+  images: PropTypes.string,
 };
 
 export default Item;
