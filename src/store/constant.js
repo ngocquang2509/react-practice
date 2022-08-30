4dd508f8d0dbba6287043eab6a302069dbfcf467
ngocquang2509 <ngocquang2509@gmail.com>
2022-08-30 14:08:22 +0700

    Update select menu #34

--- src/store/constant.js
+++ src/store/constant.js
@@ -4,8 +4,8 @@ export const DELETE_PRODUCT = "delete-product";
 
 export const selectCategory = [
     {id: 1, value: '', label: 'Select Category'},
-    {id: 2, value: 'shoes', label: 'Shoes'},
-    {id: 3, value: 'shirt', label: 'Shirt'},
-    {id: 4, value: 'jeans', label: 'Jeans'},
-    {id: 5, value: 'accesories', label: 'Accesories'},
+    {id: 2, value: 'Shoes', label: 'Shoes'},
+    {id: 3, value: 'Shirt', label: 'Shirt'},
+    {id: 4, value: 'Jeans', label: 'Jeans'},
+    {id: 5, value: 'Accesories', label: 'Accesories'},
 ]
